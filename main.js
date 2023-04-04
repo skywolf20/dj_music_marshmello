c7889bf1c82aa03e8bcd02755270970944d645f3
skywolf20 <76024016+skywolf20@users.noreply.github.com>
2023-04-04 17:14:59 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ leftWristX= 0;
 leftWirstY= 0;
 rightWristX= 0;
 rightWristY= 0;
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
 function preload()
 {
     song = loadSound("music.mp3");
@@ -16,12 +18,26 @@ function setup()
     video = createCapture(VIDEO);
     video.hide();
 
-    posNet = ml5.poseNet(video, modelLoaded);
+    poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
 function draw() 
 {
 image(video, 0, 0, 600, 500);    
+
+fill("#FF0000");
+stroke("#FF0000");
+
+if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX,leftWristY,20);
+        InNumberleftWristY = Number(leftWristY);
+        new_leftWristY = floor(InNumberleftWristY *2);
+        leftWristY_divide_1000 = new_leftWristY/1000;
+        document.getElementById("volume").innerHTML="volume"+leftWristY_divide_1000;
+        song.setVolume(leftWristY_divide_1000);
+
+    }
 }
 function play()
 {
@@ -39,6 +55,7 @@ function gotPoses(results)
 if(results.length > 0)
 {
 console.log(results);
+scoreLeftWrist = results[0].pose.keypoints[9].score;
 leftWristX = results[0].pose.leftWrist.x;
 leftWristY = results[0].pose.leftWrist.y;
 
