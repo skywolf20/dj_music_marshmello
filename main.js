6e3a679092ce4cdddc4be4c92a08ba1daa725633
skywolf20 <76024016+skywolf20@users.noreply.github.com>
2023-04-04 17:43:54 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -21,13 +21,46 @@ function setup()
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
-function draw() 
+function draw()
 {
 image(video, 0, 0, 600, 500);    
 
 fill("#FF0000");
 stroke("#FF0000");
+if(scoreRightWrist > 0.2)
+    { 
+        circle(rightWristX,rightWristY,20);
 
+        if(rightWristY >0 && rightWristY <= 100)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 0.5x";        
+            song.rate(0.5);
+        }
+else if(rightWristY >100 && rightWristY <= 200)
+{
+    document.getElementById("speed").innerHTML = "Speed = 1x";        
+            song.rate(1);
+
+}
+else if(rightWristY >200 && rightWristY <= 300)
+{
+    document.getElementById("speed").innerHTML = "Speed = 1.5x";        
+            song.rate(1.5);
+
+}
+else if(rightWristY >300 && rightWristY <= 400)
+{
+    document.getElementById("speed").innerHTML = "Speed = 2x";        
+            song.rate(2);
+
+}
+else if(rightWristY >400)
+{
+    document.getElementById("speed").innerHTML = "Speed = 2.5x";        
+            song.rate(2.5);
+
+}
+    }
 if(scoreLeftWrist > 0.2)
     {
         circle(leftWristX,leftWristY,20);
@@ -36,6 +69,7 @@ if(scoreLeftWrist > 0.2)
         leftWristY_divide_1000 = new_leftWristY/1000;
         document.getElementById("volume").innerHTML="volume"+leftWristY_divide_1000;
         song.setVolume(leftWristY_divide_1000);
+
 
     }
 }
